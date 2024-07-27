e0eeba5f353b3c9137cf96e0a9092036fba4ea2e
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-07-27 22:07:44 +0530

    image count added

--- backend/routes/imagesRoutes.js
+++ backend/routes/imagesRoutes.js
@@ -1,4 +1,4 @@
-const {addImage,getAllImages,getImage,deleteImage,updateImage } = require('../controllers/imageController')
+const {addImage,getAllImages,getImage,deleteImage,updateImage,getAllImagesCount } = require('../controllers/imageController')
 const { imageUpload } = require('../middleware/imageUpload')
 const router = require('express').Router()
 const authMiddleware = require('../middleware/authMiddleware.js');
@@ -8,11 +8,14 @@ const authMiddleware = require('../middleware/authMiddleware.js');
 router.post('/image/upload',authMiddleware.authenticationMiddleware,authMiddleware.superAdminMiddleware, imageUpload.single('image'), addImage)
 router.delete('/image/:id',authMiddleware.authenticationMiddleware,authMiddleware.superAdminMiddleware, deleteImage)
 router.put('/image/:id',authMiddleware.authenticationMiddleware,authMiddleware.superAdminMiddleware, updateImage)
+router.get('/image/count',authMiddleware.authenticationMiddleware,authMiddleware.superAdminMiddleware, getAllImagesCount)
 
 // Admin middleware
 router.post('/image/upload',authMiddleware.authenticationMiddleware,authMiddleware.adminMiddleware, imageUpload.single('image'), addImage)
 router.delete('/image/:id',authMiddleware.authenticationMiddleware,authMiddleware.adminMiddleware, deleteImage)
 router.put('/image/:id',authMiddleware.authenticationMiddleware,authMiddleware.adminMiddleware, updateImage)
+router.get('/image/count',authMiddleware.authenticationMiddleware,authMiddleware.adminMiddleware, getAllImagesCount)
+
 
 // User middleware
 router.get('/images', getAllImages)
