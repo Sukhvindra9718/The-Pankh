4eca34c1b8df8ba0e685fb7815ea12031d7e3022
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-08-09 18:12:59 +0530

    test

--- newFrontend/src/pages/About.js
+++ newFrontend/src/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderOne from '../common/header/HeaderOne';
 import Breadcrumb from '../common/breadcrumb/Breadcrumb';
 import AboutOne from '../components/about/AboutOne';
-import ProjectOne from '../components/project/ProjectOne';
+// import ProjectOne from '../components/project/ProjectOne';
 import SkillsOne from '../components/skills/SkillOne';
 import TeamOne from '../components/team/TeamOne';
 import VideoOne from '../components/video/VideoOne';
