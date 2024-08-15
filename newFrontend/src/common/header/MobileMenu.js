17d34ac7453763f4cbca87d47e3893b6554bb085
prateekpandey123 <prateekpandey736@gmail.com>
2024-08-15 19:10:41 +0530

    remove help and search on header

--- newFrontend/src/common/header/MobileMenu.js
+++ newFrontend/src/common/header/MobileMenu.js
@@ -15,6 +15,20 @@ const menuData = [
   {
     title: "About",
     path: "/about",
+    iconClosed: <CgChevronLeft />,
+    iconOpened: <CgChevronDown />,
+
+    subNav: [
+      {
+        title: "Who We Are",
+        path: "/whoweare",
+        cName: "sub-nav",
+      },
+      {
+        title: "The Pankh Model",
+        path: "/ThePankhModel",
+      },
+    ],
   },
 
   {
@@ -62,22 +76,7 @@ const menuData = [
   },
   {
     title: "Donation",
-    path: "#",
-
-    iconClosed: <CgChevronLeft />,
-    iconOpened: <CgChevronDown />,
-
-    subNav: [
-      {
-        title: "Donation",
-        path: "/donation",
-        cName: "sub-nav",
-      },
-      {
-        title: "Donate Now",
-        path: "/donate-now",
-      },
-    ],
+    path: "/donate-now",
   },
 
   {
